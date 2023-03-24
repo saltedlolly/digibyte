beea109e4c21fc45a016bb30e7556ef6dd446a44
SmartArray <yoshijaeger@Yoshis-MacBook-Pro.local>
2023-03-24 15:38:17 +0100

    fix(icons): recreated and reconfigured icons and theme

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -67,7 +67,7 @@ SplashScreen::SplashScreen(const NetworkStyle* networkStyle)
     pixPaint.fillRect(rGradient, gradient);
 
     // draw the digibyte icon, expected size of PNG: 1024x1024
-    QRect rectIcon(QPoint(-150,-122), QSize(430,430));
+    QRect rectIcon(QPoint(-160,-122), QSize(430,430));
 
     const QSize requiredSize(1024,1024);
     QPixmap icon(networkStyle->getAppIcon().pixmap(requiredSize));
