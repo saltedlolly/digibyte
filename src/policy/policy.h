e1883fe7f8939ece183d260aba57abeac3438900
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2023-12-23 08:22:31 -0700

    Match DEFAULT_MIN_RELAY_TX_FEE

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -22,7 +22,7 @@ static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 0;
 /** Default for -blockmaxweight, which controls the range of block weights the mining code will create **/
 static const unsigned int DEFAULT_BLOCK_MAX_WEIGHT = MAX_BLOCK_WEIGHT - 4000;
 /** Default for -blockmintxfee, which sets the minimum feerate for a transaction in blocks created by mining code **/
-static const unsigned int DEFAULT_BLOCK_MIN_TX_FEE = 10000;
+static const unsigned int DEFAULT_BLOCK_MIN_TX_FEE = 100000;
 /** The maximum weight for transactions we're willing to relay/mine */
 static const unsigned int MAX_STANDARD_TX_WEIGHT = 400000;
 /** The minimum non-witness size for transactions we're willing to relay/mine (1 segwit input + 1 P2WPKH output = 82 bytes) */
