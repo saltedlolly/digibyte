8ff41df7dfaa15fac9b870988537917ed4a51f08
Jan <125610144+Jongjan88@users.noreply.github.com>
2024-02-25 18:50:06 +0100

    CFeeRate MAX_MONEY

--- src/test/amount_tests.cpp
+++ src/test/amount_tests.cpp
@@ -85,7 +85,7 @@ BOOST_AUTO_TEST_CASE(GetFeeTest)
     BOOST_CHECK(CFeeRate(CAmount(26), 789) == CFeeRate(32));
     BOOST_CHECK(CFeeRate(CAmount(27), 789) == CFeeRate(34));
     // Maximum size in bytes, should not crash
-    CFeeRate(MAX_MONEY <= uint32_t(std::numeric_limits<uint32_t>::max())).GetFeePerK();
+    CFeeRate(MAX_MONEY, std::numeric_limits<uint32_t>::max()).GetFeePerK();
 }
 
 BOOST_AUTO_TEST_CASE(BinaryOperatorTest)
